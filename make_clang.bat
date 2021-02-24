c1e142d9497d580742d5088f68f22836b0e76976
jcorks <jcorkery@umich.edu>
2021-02-23 22:40:48 -0500

    Rolled out travis ci command for windows builds so that it stops yelling at me.

--- make_clang.bat
+++ make_clang.bat
@@ -1,2 +1,2 @@
-gcc.exe tpng.c -Wall -O2 tests\driver.c -o tests\tpng_test.exe
-gcc.exe tpng.c -Wall -O2 example\helper.c example\main.c -o example\example.exe
+clang.exe tpng.c -Wall -O2 tests\driver.c -o tests\tpng_test.exe
+clang.exe tpng.c -Wall -O2 example\helper.c example\main.c -o example\example.exe
